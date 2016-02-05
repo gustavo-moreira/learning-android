6b69482321d3be4d0b3714929f11716cd13204d4
Gustavo Moreira <gustavomoreira@google.com>
2016-02-04 18:50:46 -0800

    Lesson 3, adding preferences

--- Sunshine/app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -6,24 +6,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MainActivity extends ActionBarActivity {
+public class DetailActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
+                    .add(R.id.container, new DetailActivityFragment())
                     .commit();
         }
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.detail, menu);
         return true;
     }
 
